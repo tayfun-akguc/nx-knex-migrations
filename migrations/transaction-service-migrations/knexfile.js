b6f48a277a64169cbf542d4e9e5ec355a10e5393
Tayfun <dev.akguc@gmail.com>
2024-07-15 22:10:25 +0300

    postgres configuration

--- migrations/transaction-service-migrations/knexfile.js
+++ migrations/transaction-service-migrations/knexfile.js
@@ -4,44 +4,25 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
+  local: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: process.env.TRANSACTION_SERVICE_DB_HOST,
+      port: process.env.TRANSACTION_SERVICE_DB_PORT,
+      database: process.env.TRANSACTION_SERVICE_DB_NAME,
+      user: process.env.TRANSACTION_SERVICE_DB_USER,
+      password: process.env.TRANSACTION_SERVICE_DB_PASSWORD,
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+      directory: 'migrations',
+    },
+    seeds: {
+      directory: 'seeds',
+    },
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
